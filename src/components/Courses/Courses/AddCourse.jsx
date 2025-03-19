9df8e3c82418237f65bf069ece913ec785933008
Marslino Ashraf Shnouda Sedra <marslinoashraf8@gmail.com>
2025-03-19 21:33:42 +0200

    Updated AddCourse.jsx

--- src/components/Courses/Courses/AddCourse.jsx
+++ src/components/Courses/Courses/AddCourse.jsx
@@ -1,4 +1,4 @@
-  import React, { useState } from "react";
+import React, { useState } from "react";
   import { useFormik } from "formik";
   import * as Yup from "yup";
   import { ToastContainer, toast } from "react-toastify";
@@ -84,8 +84,9 @@
           name: values.name,
           price: values.price,
           description: values.description,
-          instructorId: 4,
-          categoryId: [parseInt(categoryId)],
+          instructorId: 7,
+          categoryId: [6],
+          // categoryId: [parseInt(categoryId)],
         };
       
         const token = localStorage.getItem("token");
